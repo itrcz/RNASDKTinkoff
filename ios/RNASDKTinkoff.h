3647f25c889e42b70cfe0e7bb219dd82c2283fd2
Awesome <jleed@me.com>
2018-12-15 23:01:05 +0300

    fixes

--- ios/RNASDKTinkoff.h
+++ ios/RNASDKTinkoff.h
@@ -6,11 +6,7 @@
 //  Copyright © 2018 Ilya Trikoz. All rights reserved.
 //
 
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
 #import <React/RCTBridge.h>
-#endif
 
 @interface RNASDKTinkoff : NSObject <RCTBridgeModule>
 
